016938da29f9a1f046a845f5ae78968189ab48e5
aldvnian <138926924+aldvnian@users.noreply.github.com>
2025-03-08 11:46:18 +0000

    Update Player.py

--- Player.py
+++ Player.py
@@ -5,31 +5,40 @@ canvasWidth = 700
 canvasHeight = 600
 
 class Player:
-    def __init__(self, name, pos, shipImageURL):
+    def __init__(self, name, pos, shipImageURL, startingRotation):
         self.name = name
         self.pos = pos
         self.vel = Vector()
         self.shipImage = simplegui.load_image(shipImageURL)
         self.widthHeightDest = (100, 100)
         self.centreDest = (self.pos.get_p()[0], self.pos.get_p()[1])
-        self.current_rotation = 0
+        self.current_rotation = startingRotation
+        self.facing = "Right"
+        self.initialRot = startingRotation
 
     def draw(self, canvas):
         centre = (self.shipImage.get_width()/2, self.shipImage.get_height()/2)
         widthHeightSource = (self.shipImage.get_width(), self.shipImage.get_height())
-
         canvas.draw_image(self.shipImage, centre, widthHeightSource, self.centreDest, self.widthHeightDest, self.current_rotation)
         
-    def turn(self, degrees):
+    def turn(self, degrees, facing):
         speed = degrees/32
+        
         if self.current_rotation < degrees:
-            self.current_rotation += speed
+            self.current_rotation -= speed
             if self.current_rotation > degrees:
                 self.current_rotation = degrees
+                self.facing = facing
         elif self.current_rotation > degrees:
             self.current_rotation += speed
             if self.current_rotation < degrees:
                 self.current_rotation = degrees
+                self.facing = facing
+                
+    def update(self):
+        self.pos.add(self.vel)
+        self.centreDest = (self.pos.get_p()[0], self.pos.get_p()[1])
+        self.vel.multiply(0.85)
                 
 class Keyboard:
     def __init__(self):
@@ -46,23 +55,23 @@ class Keyboard:
         if key == simplegui.KEY_MAP['right']:
             if self.left == True:
                 self.left = False
-                self.right = True
-                self.rightKeyDown = True
+            self.right = True
+            self.rightKeyDown = True
         elif key == simplegui.KEY_MAP['left']:
             if self.right == True:
                 self.right = False
-                self.left = True
-                self.leftKeyDown = True
+            self.left = True
+            self.leftKeyDown = True
         elif key == simplegui.KEY_MAP['up']:
             if self.down == True:
                 self.down = False
-                self.up = True
-                self.upKeyDown = True
+            self.up = True
+            self.upKeyDown = True
         elif key == simplegui.KEY_MAP['down']:
             if self.up == True:
                 self.up = False
-                self.down = True
-                self.downKeyDown = True
+            self.down = True
+            self.downKeyDown = True
 
     def keyUp(self, key):
         if key == simplegui.KEY_MAP['right']:
@@ -91,19 +100,23 @@ class Interaction:
         self.player = player
         self.keyboard = keyboard
         
-    def update(self):
+    def update(self, canvas):
         if self.keyboard.up:
-            print("hi")
-            self.player.vel.add(0, -1)
-            self.player.turn(math.pi/2)
+            self.player.vel.add(Vector(0, -1))
+            if not self.player.facing == "Up":
+                self.player.turn(-math.pi/2, "Up")
         if self.keyboard.down:
-            self.player.vel.add(0, 1)
-            self.player.turn(-math.pi/2)
+            self.player.vel.add(Vector(0, 1))
+            if not self.player.facing == "Down":
+                self.player.turn(math.pi/2, "Down")
         if self.keyboard.left:
-            self.player.vel.add(-1, 0)
-            self.player.turn(math.pi)
+            self.player.vel.add(Vector(-1, 0))
+            if not self.player.facing == "Left":
+                self.player.turn(-math.pi, "Left")
         if self.keyboard.right:
-            self.player.vel.add(1, 0)
+            self.player.vel.add(Vector(1, 0))
+            if not self.player.facing == "Right":
+                self.player.turn(math.pi, "Right")
 
 player = Player("Adnan", 
                 Vector(canvasWidth/2, canvasHeight - 100), 
@@ -112,7 +125,8 @@ kbd = Keyboard()
 inter = Interaction(player, kbd)
 
 def draw_handler(canvas):
-    inter.update()
+    inter.update(canvas)
+    player.update()
     player.draw(canvas)
 
 frame = simplegui.create_frame('Player', canvasWidth, canvasHeight)
