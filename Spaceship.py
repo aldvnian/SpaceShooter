345fa8eabb13872752ed57c7dd468211266a3c73
aldvnian <138926924+aldvnian@users.noreply.github.com>
2025-03-14 18:55:26 +0000

    Create Spaceship Class
    Enemy class and Player class should inherit from the spaceship class and override it appropriately

---
+++ Spaceship.py
@@ -0,0 +1,21 @@
+class Spaceship():
+  def __init__(self, shipImageURL, startingRotation, canvasWidth, canvasHeight):
+    self.pos = Vector(canvasWidth/2, canvasHeight/2)
+    self.vel = Vector()
+    self.shipImage = simplegui.load_image(shipImageURL)
+    self.widthHeightDest = (70, 70)
+    self.centreDest = (self.pos.get_p()[0], self.pos.get_p()[1])
+    self.currentRotation = math.pi/2
+
+  def draw(self, canvas):
+    centre = (self.shipImage.get_width()/2, self.shipImage.get_height()/2)
+    widthHeightSource = (self.shipImage.get_width(), self.shipImage.get_height())
+    canvas.draw_image(self.shipImage, centre, widthHeightSource, self.centreDest, self.widthHeightDest, self.currentRotation)
+
+  def move(self, coordinate):
+    self.centreDest = coordinate
+
+  def update(self):
+    self.pos.add(self.vel)
+    self.centreDest = (self.pos.get_p()[0], self.pos.get_p()[1])
+    self.vel.multiply(0.73)
