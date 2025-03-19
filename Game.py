9bc6b61ec6376fe7fb9a5b6af7ba993e327abb64
aldvnian <138926924+aldvnian@users.noreply.github.com>
2025-03-19 13:56:09 +0000

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,1 +1,6 @@
-
+class Game:
+    def __init__(self):
+        self.player = Player("https://aldvnian.github.io/Spaceshooter-sprites/craftpix-991101-free-pixel-art-enemy-spaceship-2d-sprites/PNG_Parts%26Spriter_Animation/Ship6/Ship6.png",
+               		  -math.pi/2, canvasWidth, canvasHeight)
+        self.keyboard = Keyboard()
+        self.inter = Interaction(self.player, self.keyboard)
