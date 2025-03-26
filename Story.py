90221a7104d435b67d166c41bd55fde4c9a34300
SoftwareWill <152651424+SoftwareWill@users.noreply.github.com>
2025-03-26 11:33:32 +0000

    Update Story.py

--- Story.py
+++ Story.py
@@ -1,16 +1,13 @@
 import simplegui
 
-WIDTH, HEIGHT = 500, 400
-
-
 class Story:
     
 
     def __init__(self):
-      
+        self.canvas_width = 800
+        self.canvas_height = 650
         self.background_url = "https://t3.ftcdn.net/jpg/01/94/53/22/360_F_194532293_5DQuTyT4ni7eCuVvifJgkMRNi92CoTjk.jpg"
         self.background = simplegui.load_image(self.background_url)
-
         self.title = "Space Raider Origin Story"
         self.title_size = 32
         self.title_font = "monospace"
@@ -27,39 +24,37 @@ class Story:
 
        
         self.button_width, self.button_height = 100, 40
-        self.button_x = (WIDTH - self.button_width) // 2
+        self.button_x = (self.canvas_width - self.button_width) // 2
         self.button_y = 240
         self.button_text = "Play"
 
         
-        self.frame = simplegui.create_frame("Story", WIDTH, HEIGHT)
+        self.frame = simplegui.create_frame("Story", self.canvas_width, self.canvas_height)
         self.frame.set_draw_handler(self.draw)
         self.frame.set_mouseclick_handler(self.click)
-
+        
     def draw(self, canvas):
-        
-        
-        if self.background.get_width() > 0 and self.background.get_height() > 0:
+        if self.background.get_width() > 0:
             canvas.draw_image(
-                self.background,
-                (self.background.get_width() / 2, self.background.get_height() / 2),
-                (self.background.get_width(), self.background.get_height()),
-                (WIDTH / 2, HEIGHT / 2),
-                (WIDTH, HEIGHT)
+                self.background, 
+                (self.background.get_width()/2, self.background.get_height()/2), 
+                (self.background.get_width(), self.background.get_height()), 
+                (self.canvas_width/2, self.canvas_height/2), 
+                (self.canvas_width, self.canvas_height)
             )
 
-        # Draw title
+        
         title_width = self.frame.get_canvas_textwidth(self.title, self.title_size, self.title_font)
-        canvas.draw_text(self.title, [(WIDTH - title_width) // 2, 40], self.title_size, "White", self.title_font)
+        canvas.draw_text(self.title, [(self.canvas_width - title_width) // 2, 40], self.title_size, "White", self.title_font)
 
-        # Draw story text
+        
         y_offset = 90
         for line in self.story_text:
             text_width = self.frame.get_canvas_textwidth(line, 18, "serif")
-            canvas.draw_text(line, [(WIDTH - text_width) // 2, y_offset], 18, "White", "serif")
+            canvas.draw_text(line, [(self.canvas_width - text_width) // 2, y_offset], 18, "White", "serif")
             y_offset += 25
 
-            # Draw start button
+            
         canvas.draw_polygon(
             [(self.button_x, self.button_y), (self.button_x + self.button_width, self.button_y),
              (self.button_x + self.button_width, self.button_y + self.button_height),
@@ -67,7 +62,7 @@ class Story:
             2, "White", "Gray"
         )
 
-        # Draw button text
+        
         text_width = self.frame.get_canvas_textwidth(self.button_text, 20, self.title_font)
         canvas.draw_text(self.button_text,
                          [(self.button_x + (self.button_width - text_width) / 2), self.button_y + 28],
@@ -85,6 +80,6 @@ class Story:
         
 
 
-# Create and start the story screen
+
 StoryScreen = Story()
 StoryScreen.frame.start()
