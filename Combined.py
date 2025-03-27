73d77e5bb371eeead243027527c76beee5530b10
SoftwareWill <152651424+SoftwareWill@users.noreply.github.com>
2025-03-27 17:44:42 +0000

    Update Combined.py

--- Combined.py
+++ Combined.py
@@ -787,4 +787,79 @@ def pauseMusic():
 timer = simplegui.create_timer(musicDuration, restartMusic)
 playMusic()
 
-menu = Menu()
+class Story:
+     def __init__(self):
+         self.canvas_width = 800
+         self.canvas_height = 650
+         self.background_url = "https://t3.ftcdn.net/jpg/01/94/53/22/360_F_194532293_5DQuTyT4ni7eCuVvifJgkMRNi92CoTjk.jpg"
+         self.background = simplegui.load_image(self.background_url)
+         self.title = "Space Raider Origin Story"
+         self.title_size = 45
+         self.title_font = "monospace"
+ 
+         self.story_text = [
+             "In the year 4035",
+             "The Aliens have invaded our universe",
+             "The Galatic Defense Federation is falling",
+             "You are humanity's last flame of hope",
+             "Pilot your trusty spaceship and save humanity from the aliens",
+             "and uncover the truth behind their invasion...",
+         ]
+         self.button_width = 150
+         self.button_height = 50
+         self.button_x = (self.canvas_width - self.button_width) // 2
+         self.button_y = 500
+         self.button_text = "Play"
+         self.button_text_size = 32
+         
+         self.frame = simplegui.create_frame("Story", self.canvas_width, self.canvas_height)
+         self.frame.set_draw_handler(self.draw)
+         self.frame.set_mouseclick_handler(self.click)
+         
+     def draw(self, canvas):
+         if self.background.get_width() > 0:
+             canvas.draw_image(
+                 self.background, 
+                 (self.background.get_width()/2, self.background.get_height()/2), 
+                 (self.background.get_width(), self.background.get_height()), 
+                 (self.canvas_width/2, self.canvas_height/2), 
+                 (self.canvas_width, self.canvas_height)
+             )
+ 
+         
+         title_width = self.frame.get_canvas_textwidth(self.title, self.title_size, self.title_font)
+         canvas.draw_text(self.title, [(self.canvas_width - title_width) // 2, 40], self.title_size, "White", self.title_font)
+ 
+         
+         y_offset = 200
+         for line in self.story_text:
+             text_width = self.frame.get_canvas_textwidth(line, 20, "serif")+ 20
+             canvas.draw_text(line, [(self.canvas_width - text_width) // 2, y_offset], 22, "White", "serif")
+             y_offset += 30
+ 
+             
+         canvas.draw_polygon(
+             [(self.button_x, self.button_y), (self.button_x + self.button_width, self.button_y),
+              (self.button_x + self.button_width, self.button_y + self.button_height),
+              (self.button_x, self.button_y + self.button_height)],
+             2, "White", "Gray"
+         )
+ 
+         
+         text_width = self.frame.get_canvas_textwidth(self.button_text, self.button_text_size, self.title_font)
+         canvas.draw_text(self.button_text,
+                  [(self.button_x + (self.button_width - text_width) / 2), self.button_y + (self.button_height // 2) + (self.button_text_size // 3)],  
+                  self.button_text_size, "White", self.title_font)
+ 
+     def click(self, pos):
+        
+         x, y = pos
+         if self.button_x <= x <= self.button_x + self.button_width and self.button_y <= y <= self.button_y + self.button_height:
+             self.frame.stop()
+             menu = Menu()
+             
+ 
+ 
+ 
+StoryScreen = Story()
+StoryScreen.frame.start()
